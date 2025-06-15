17c6a6f7023e94f03903f9198f0e832bcbe288f6
Patrick <patrickmhadiwinoto@gmail.com>
2025-06-15 17:32:20 -0400

    Login and Signup Page
    Not Completed, Still Needs Tweeking

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Import From Other Files
 
@@ -25,6 +26,14 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors({
+
+    origin: "http://localhost:5173",
+
+    credentials: true,
+
+}));
 
 app.use("/api/auth", authRoutes);
 
