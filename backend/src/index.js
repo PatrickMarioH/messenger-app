8c220e92b932adba0b76cc1e71328ef7a255864d
Patrick <patrickmhadiwinoto@gmail.com>
2025-06-28 11:30:04 -0400

    Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,8 @@ import messageRoutes from "./routes/message.route.js";
 
 import { app, server } from "./lib/socket.js";
 
+import path from "path"
+
 // Initialization
 
 dotenv.config();
@@ -22,6 +24,8 @@ dotenv.config();
 // Main
 
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 
@@ -40,6 +44,18 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+
+    });
+    
+}
 
 server.listen(PORT, () => {
 
